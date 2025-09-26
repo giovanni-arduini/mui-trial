a4f45fd84d9bc3a9449e667b59761e95af6387e9
giovanni-arduini <gionnibigudd@gmail.com>
2025-09-26 11:17:52 +0200

    ADD App structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,58 @@
+import {
+  Button,
+  Typography,
+  AppBar,
+  CssBaseline,
+  Toolbar,
+  Container,
+} from "@mui/material";
+
+import Grid from "@mui/material/Grid";
+
+import { PhotoCamera } from "@mui/icons-material";
+
+import Navbar from "./components/NavBar/NavBar.tsx";
+
 export default function App() {
   return (
     <>
-      <div>
-        <h1>Hello World!</h1>
-      </div>
+      <CssBaseline />
+      {/* <AppBar position="relative">
+        <Toolbar>
+          <PhotoCamera />
+          <Typography variant="h6">Photo Album</Typography>
+        </Toolbar>
+      </AppBar> */}
+      <Navbar />
+      <main>
+        <Container maxWidth="sm">
+          <Typography
+            variant="h2"
+            align="center"
+            color="textPrimary"
+            gutterBottom
+          >
+            Photo Album
+          </Typography>
+          <Typography variant="h5" align="center" color="textSecondary">
+            Hello everyone, this is a photo album and I'm trying to make this
+            sentence as long as possible, so we can see how it looks like on the
+            screen
+          </Typography>
+          <Grid container spacing={2} justifyContent="center">
+            <Grid>
+              <Button variant="contained" color="primary">
+                See my photos
+              </Button>
+            </Grid>
+            <Grid>
+              <Button variant="outlined" color="primary">
+                Secondary action
+              </Button>
+            </Grid>
+          </Grid>
+        </Container>
+      </main>
     </>
   );
 }
