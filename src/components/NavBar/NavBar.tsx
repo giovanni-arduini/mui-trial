7bb3cc7c0f9d4068df658c35bcef3b83bf592068
giovanni-arduini <gionnibigudd@gmail.com>
2025-09-26 11:18:02 +0200

    ADD NavBar MUI component

---
+++ src/components/NavBar/NavBar.tsx
@@ -0,0 +1,104 @@
+import { useState } from "react";
+
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { Coffee, Menu } from "@mui/icons-material";
+import {
+  Button,
+  useTheme,
+  Drawer,
+  Box,
+  List,
+  ListItem,
+  ListItemText,
+  ListItemButton,
+  IconButton,
+} from "@mui/material";
+import { useMediaQuery } from "@mui/system";
+
+const Navbar = () => {
+  const [drawerOpen, setDrawerOpen] = useState(false);
+  const theme = useTheme();
+
+  const isMobile = useMediaQuery(theme.breakpoints.down("md"));
+
+  const toggleDrawer = (open: boolean) => {
+    setDrawerOpen(open);
+  };
+
+  const drawerLinks = [
+    {
+      text: "Home",
+      link: "#home",
+    },
+    {
+      text: "Coffee",
+      link: "#coffee",
+    },
+    {
+      text: "Info",
+      link: "#info",
+    },
+  ];
+
+  return (
+    <>
+      <AppBar position="sticky" color="primary">
+        <Container maxWidth="lg">
+          <Toolbar>
+            <Coffee />
+            <Typography variant="h5" sx={{ flexGrow: 1 }}>
+              Coffee Shop
+            </Typography>
+
+            {isMobile ? (
+              <IconButton color="inherit" onClick={() => toggleDrawer(true)}>
+                <Menu />
+              </IconButton>
+            ) : (
+              <>
+                <Button color="inherit" href="#coffee">
+                  COFFEE
+                </Button>
+                <Button color="inherit" href="#home">
+                  HOME
+                </Button>
+              </>
+            )}
+          </Toolbar>
+        </Container>
+      </AppBar>
+
+      <Drawer
+        anchor="right"
+        open={drawerOpen}
+        onClose={() => toggleDrawer(false)}
+      >
+        <Box
+          sx={{ width: 200 }}
+          role="presentation"
+          onClick={() => toggleDrawer(false)}
+        >
+          <List>
+            {drawerLinks.map((linkItem, index) => {
+              return (
+                <ListItem key={index} disablePadding>
+                  <ListItemButton
+                    component="a"
+                    href={linkItem.link}
+                    onClick={() => toggleDrawer(false)}
+                  />
+                  <ListItemText primary={linkItem.text} />
+                </ListItem>
+              );
+            })}
+          </List>
+        </Box>
+      </Drawer>
+    </>
+  );
+};
+
+export default Navbar;
